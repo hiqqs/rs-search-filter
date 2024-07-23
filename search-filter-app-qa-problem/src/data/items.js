7470c75190cdae67c4bf2b051e4538463cb5713d
John Ahigian <jahigian@gmail.com>
2024-07-23 00:50:07 -0400

    more mock data for items and playwright config

--- search-filter-app-qa-problem/src/data/items.js
+++ search-filter-app-qa-problem/src/data/items.js
@@ -7,6 +7,12 @@ const items = [
   { id: 5, name: 'Headphones', category: 'electronics', price: 199, rating: 4.3 },
   { id: 6, name: 'painting', category: 'art', price: 75, rating: 4.3 },
   { id: 7, name: 'Drill', category: 'tools', price: 250, rating: 3.9 },
+  { id: 8, name: 'NVIDIA 4090', category: 'electronics', price: 200, rating: 3.2 },
+  { id: 9, name: 'NVIDIA 4090.1', category: 'electronics', price: 200, rating: 3.0 },
+  { id: 10, name: '$NVDA', category: 'electronics', price: 100, rating: 2.0 },
+  { id: 11, name: 'Sonoma', category: 'software', price: 150, rating: 4.8 },
+  { id: 12, name: 'Yosemite', category: 'software', price: 149, rating: 1.8 },
+  { id: 13, name: 'Crowdstrike', category: 'software', price: 129, rating: 0 },
 ];
 
 export default items;
